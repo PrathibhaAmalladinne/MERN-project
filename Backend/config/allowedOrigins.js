b3e9784e4d82fdddfc0c3f8892878af38e576698
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-11 17:22:06 +0530

    allowed origins

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://ziraa.netlify.app/",
-  "https://post-it-cbah.onrender.com",
+  // "https://ziraa.netlify.app/",
+  // "https://post-it-cbah.onrender.com",
+  "https://mern-project-j7gj.vercel.app/",
 ]
 
 module.exports = allowedOrigins
