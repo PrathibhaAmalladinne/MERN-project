1fca5a06803c5362159bbf5d43b9b79b066bf409
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-12 13:03:35 +0530

    0.0.0.0

--- Backend/server.js
+++ Backend/server.js
@@ -49,7 +49,9 @@ app.use(errorHandler)
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB")
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  app.listen(PORT, "0.0.0.0", () =>
+    console.log(`Server running on port ${PORT}`)
+  )
 })
 
 mongoose.connection.on("error", (err) => {
