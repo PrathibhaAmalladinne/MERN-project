c048cf9439e5ba0c0f404175a15e07079c540301
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-12 12:26:55 +0530

    remove 404

--- Backend/server.js
+++ Backend/server.js
@@ -34,16 +34,16 @@ connectDB()
 //   connectTimeoutMS: 60000,
 // })
 
-app.all("*", (req, res) => {
-  res.status(404)
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"))
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 NOT FOUND" })
-  } else {
-    res.type("txt").send("404 Not Found")
-  }
-})
+// app.all("*", (req, res) => {
+//   res.status(404)
+//   if (req.accepts("html")) {
+//     res.sendFile(path.join(__dirname, "views", "404.html"))
+//   } else if (req.accepts("json")) {
+//     res.json({ message: "404 NOT FOUND" })
+//   } else {
+//     res.type("txt").send("404 Not Found")
+//   }
+// })
 
 app.use(errorHandler)
 
