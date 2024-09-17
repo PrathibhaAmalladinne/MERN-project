bc47808e307d9a7fed36b42a3a72248fa159e635
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-17 13:27:09 +0530

    cors origin

--- Backend/config/corsOptions.js
+++ Backend/config/corsOptions.js
@@ -1,13 +1,14 @@
-const allowedOrigins = require("./allowedOrigins")
+// const allowedOrigins = require("./allowedOrigins")
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
+  // origin: (origin, callback) => {
+  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true)
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"))
+  //   }
+  // },
+  origin: ["https://mern-post-it.vercel.app/", "http://localhost:5173"],
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE"],
