d7438f904280e3979ff4d9f4d944a9280b724302
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-17 12:54:49 +0530

    correcting url names

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,10 +1,6 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:3500",
-  // "https://ziraa.netlify.app/",
-  // "https://post-it-cbah.onrender.com",
-  "https://mern-project-j7gj.vercel.app",
-  // "https://mern-project-three-tau.vercel.app/",
+  "https://mern-post-it-api.vercel.app/",
 ]
 
 module.exports = allowedOrigins
--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -6,8 +6,8 @@ const useApi = () => {
   const { currentToken, token, setCredentials } = useAuth()
   // const[currentToken,setCurrenToken] = useState[token]
   const instance = axios.create({
-    // baseURL: "https://post-it-api-a1t6.onrender.com",
-    baseURL: "http://localhost:3500",
+    baseURL: "https://mern-post-it-api.vercel.app/",
+    // baseURL: "http://localhost:3500",
     // baseURL: "https://mern-project-three-tau.vercel.app/",
     withCredentials: true,
     // timeout: 3000,
