00c11e8203bab691d4ba2b1c303bde2e03a6508f
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-17 11:41:11 +0530

    login rem

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,6 @@ function Login() {
   const [username, setUsername] = useState("test")
   const [password, setPassword] = useState("test123")
   const [errMsg, setErrMsg] = useState("")
-  // const [persist, setPersist] = usePersist()
 
   useEffect(() => {
     userRef.current.focus()
@@ -19,42 +18,34 @@ function Login() {
   useEffect(() => {
     setErrMsg("")
   }, [username, password])
-  // const handleToggle = () => {
-  //   setPersist((prev) => !prev)
-  // }
   const loginMutation = useLogin()
   const credentials = { username, password }
   const handleLogin = async (e) => {
     e.preventDefault()
-    try {
-      await loginMutation.mutateAsync(credentials)
-      setUsername("")
-      setPassword("")
-      navigate("/dash")
-    } catch (err) {
-      if (!err.response) {
-        setErrMsg("No Server Response")
-      } else if (err.response.status === 400) {
-        setErrMsg("Missing Username or Password")
-      } else if (err.response.status === 401) {
-        setErrMsg("Unauthorized")
-      } else {
-        setErrMsg(err.response.data?.message)
-      }
-      errRef.current.focus()
-    }
+    // try {
+    //   await loginMutation.mutateAsync(credentials)
+    //   setUsername("")
+    //   setPassword("")
+    navigate("/dash")
+    // } catch (err) {
+    //   if (!err.response) {
+    //     setErrMsg("No Server Response")
+    //   } else if (err.response.status === 400) {
+    //     setErrMsg("Missing Username or Password")
+    //   } else if (err.response.status === 401) {
+    //     setErrMsg("Unauthorized")
+    //   } else {
+    //     setErrMsg(err.response.data?.message)
+    //   }
+    //   errRef.current.focus()
+    // }
   }
   return (
     <section className={styles.login}>
       <p ref={errRef} aria-live="assertive" className={styles.errmsg}>
         {errMsg}
       </p>
-      {/* <h1>Login</h1> */}
-      <form
-        className={styles.form}
-        onSubmit={handleLogin}
-        // onSubmit={() => navigate("/dash")}
-      >
+      <form className={styles.form} onSubmit={handleLogin}>
         <div className={styles.row}>
           <label htmlFor="username">Username: </label>
           <input
@@ -78,16 +69,6 @@ function Login() {
         <div>
           <button className={styles.btn}>Login</button>
         </div>
-        {/* <label htmlFor="persist">
-          <input
-            type="checkbox"
-            className={styles.checkboxcontainer}
-            id="persist"
-            onChange={handleToggle}
-            checked={persist}
-          />
-          Trust this device
-        </label> */}
       </form>
     </section>
   )
